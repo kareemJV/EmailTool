064bacd54642ac7ee7886fbd647db16cd6d1cfc8
kareemJV <61757906+kareemJV@users.noreply.github.com>
2023-05-17 23:45:49 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        EmailTool.Senden();
+        for (int i = 0; i <10 ; i++) {
+            EmailTool.schicken();
+        }
+
+
+    }
+}
